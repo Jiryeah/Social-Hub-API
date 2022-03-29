353e6f3d7d5f70f1abef411b3fbf5bf1a7ec5144
Chidori <business.faissal@outlook.com>
2022-03-29 03:28:09 -0500

    created routes

---
+++ routes/api/users-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/users-controller');
+
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+router.router('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
